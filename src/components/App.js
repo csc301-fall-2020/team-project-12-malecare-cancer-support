2e3b289499d31ccd4114b547f8a74408422a6e4a
uttkarsh berwal <uttkarsh.berwal@mail.utoronto.ca>
2020-10-31 05:41:59 +0530

    app starter files

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App(){
+    return <div>
+            <h1>Hello Team!</h1>
+        </div>
+    }
+
+export default App;    
