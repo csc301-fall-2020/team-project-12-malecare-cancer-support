1b6332f26db35d5324d8f11550d19580831b5764
Jonathan Tang <jonathanj.tang@mail.utoronto.ca>
2020-11-21 23:18:49 -0500

    Set up basic React app using create-react-app

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Basic React app template
+      </header>
+    </div>
+  );
+}
+
+export default App;
