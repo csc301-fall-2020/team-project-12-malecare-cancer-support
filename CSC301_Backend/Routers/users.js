c49b1bb3ebabcf007d5d8e9091dfdb3d01be928e
oduozaok <paschal.oduoza@mail.utoronto.ca>
2020-11-24 23:14:58 -0500

    Made few changes on the folder structure, implemented the authentication, login, signup, jwt tokens, deleted all passports

---
+++ CSC301_Backend/Routers/users.js
@@ -0,0 +1,84 @@
+/**
+ * This will contain routes related to users i.e registration or login
+ *
+ * */
+
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const User = require('../models/users');
+const {registerValidation, loginValidation} = require('../validation');
+
+
+//Registration route
+router.post('/signup',   async (req, res) => {
+
+    // Validating data of the user before submission
+    let {error} = registerValidation(req.body);
+    if (error)
+        return res.status(400).send(error.details[0].message);
+
+    //checking if the user is already in database
+    let emailExist = await User.findOne({email: req.body.email});
+    if (emailExist) {
+        return res.status(400).send('Email address already exists');
+    }
+
+    //generate a hashed password
+    console.log('Hello World');
+    const saltRounds = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, saltRounds);
+
+    // creates user with a hashed password
+    //  more objects can be passed in here
+    const user = new User({
+        email: req.body.email,
+        firstname: req.body.firstname,
+        lastname: req.body.lastname,
+        password: hashedPassword
+    });
+    try {
+        const savedUser = await user.save();
+        res.send(savedUser);
+    }
+    catch (err) {
+        res.status(400).send(err);
+    }
+});
+
+//login route
+router.post('/login', async (req, res) => {
+    //Validating login registration details
+    let {error} = loginValidation(req.body);
+    if (error)
+        return res.status(400).send(error.details[0].message);
+
+    let user = await User.findOne({email: req.body.email});
+    // if the email does not exist
+    if (!user) {
+        return res.status(400).send('Email address or Password is wrong');
+    }
+
+    //check if password is correct
+    const validPassword = await bcrypt.compare(req.body.password, user.password);
+    if (!validPassword)
+        return res.status(400).send('Email address or password is wrong');
+
+    //create token
+    const accessToken = jwt.sign(
+        {userId: user._id.toString()},
+        'SECRET_TOKEN',
+        {expiresIn: '1h'}
+    );
+    res.status(200).json({
+        accessToken
+    });
+
+    //do something here. Redirect to the main screen which I do not know yet
+    res.send('We are logged in');
+});
+
+module.exports = router;
+
--- CSC301_Backend/server/index.js
+++ CSC301_Backend/server/index.js
@@ -1,86 +1,35 @@
 const express = require("express");
 const app = express();
+const bodyParser = require('body-parser');
+
+
 //----------------------------------------
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
 
-const crypto = require('crypto');
-const createHash = crypto.createHash;
+const user = require('../Routers/users');
 
-mongoose.connect('mongodb://localhost:27017/cancer', {useNewUrlParser: true, useUnifiedTopology: true});
 
-const UserSchema = new Schema({
-    email: String,
-    displayName: String, //can be changed to full name
-    hashedPassword: String,
-    salt: String,
-
-});
-
-UserSchema //for setting and getting the password
-    .virtual('password')
-    .set(function(password){
-        this._password = password;
-        this.salt = this.makeSalt();
-        this.hashedPassword = this.encryptPassword(password);
-    })
-    .get(function(){
-        return this._password;
-    });
-
-UserSchema.methods = {
-
-    makeSalt: function(){ //makes the random salt code
-        return crypto.randomBytes(16).toString('base64');
-    },
-
-    authenticate: function(plainText) {
-        return this.encryptPassword(plainText) == this.hashedPassword; //authenticates the password using the database
-    },
-
-    encryptPassword: function(password){ //function to encrypt the password
-        if(!password || !this.salt) return '';
-       // var salt = new Buffer(this.salt, 'base64');
-       var salt = Buffer.from(this.salt, 'base64');
-        return crypto.pbkdf2Sync(password, salt, 10000, 64, 'sha512').toString('base64');
-    }
-
-};
-
-const User = mongoose.model('User',UserSchema);
-
-//const kitty = new Cat({ name: 'Zildjian' });
-//kitty.save().then(() => console.log('meow'));
-//---------------------------------------------
-
-const seed = () => {
-    //User.find({}).remove().then(()=>{
-    User.find({}).deleteMany().then(()=>{    
-        const users = [{
-            email: 'alice@example.com',
-            displayName: 'Alice',
-            password: '123123',
-        }, {
-            email: 'bob@example.com',
-            displayName: 'Bob',
-            password: '321321',
-            
-        }];
-
-        User.create(users, (err, users_) =>{//creates users
-            console.log('MONGODB SEED: '+  users_.length +' Users created.');
-        });
-
-    }); //query all users like in sql
-};
+mongoose.connect('mongodb://localhost:27017/cancer',
+    {useNewUrlParser: true, useUnifiedTopology: true},
+    () => console.log('connected to db')
+    );
 
 //--------------------------------------------
-app.get('/', function(req,res){
-    User.find({}, (err, users) => {
-        res.json(users);
-    });
+
+
+// for communication between front end & backend from different servers
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    next();
 });
 
-seed();
 
-app.listen(3000);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use('/auth', user);
+
+app.listen(3000, () => console.log('Success, Server is up and running!'));
