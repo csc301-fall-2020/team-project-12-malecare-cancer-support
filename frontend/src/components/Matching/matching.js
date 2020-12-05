7adbc2e10a8d6ad2f0c7343f14dccb4520e9b97d
Ismail Atadinc <ismail.atadinc@mail.utoronto.ca>
2020-12-05 18:54:10 -0500

    Finished switching to React Components in matching page

--- frontend/src/components/Matching/matching.js
+++ frontend/src/components/Matching/matching.js
@@ -13,241 +13,119 @@ import heartIcon from "../../images/heartIcon.svg";
 /* Landing page component */
 class Matching extends React.Component {
   render() {
-    <div class="main no-gutters">
-      <div class="col h-100">
-        <div class="row top no-gutters menuContainer">
-          <div class="col-3 navigation bigRightBorder">
-            <img class="menuIcon" src={menuIcon} />
-          </div>
-          <div class="col">
-            <div class="row logoAndMode">
-              <div class="col-7 logo">CancerChat</div>
-              <div class="col-1"></div>
-              <div class="col-1">
-                <img class="mentorIcon" src={mentorIcon} />
-              </div>
-              <div class="col-1">
-                <img class="heartIcon" src={heartIcon} />
-              </div>
-              <div class="col-2"></div>
-            </div>
-          </div>
-        </div>
-        <div class="row bottom no-gutters">
-          <div class="col-3 likes">
-            <div class="row no-gutters likesOrMessages">
-              <div class=" col-sm-12 col-md-6 no-gutters bigRightBorder">
-                <text class="center bold">
-                  Likes<text class="numLikes">1</text>
-                </text>
-              </div>
-              <div class="col-sm-12 col-md-6 no-gutters bigRightBorder">
-                <text class="center">
-                  Messages <text class="numLikes">99+</text>
-                </text>
-              </div>
-            </div>
-            <div class="likesMessages bigRightBorder">
-              Space for Likes and Messages
-            </div>
-          </div>
-          <div class="col bigLeftBorder">
-            <div class="row-10 bioAndPhoto">
-              <div class="row no-gutters h-100">
-                <div class="col-sm-12 col-lg-6 flex">
-                  <Carousel className="carousel" interval={null}>
-                    <Carousel.Item className="carouselItem">
-                      <Image
-                        className="d-block w-100 carouselImage"
-                        src="https://images.pexels.com/photos/3226584/pexels-photo-3226584.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-                        alt="First slide"
-                        fluid
-                      />
-                      <Carousel.Caption>
-                        <text className="borderLikeOrPass">PASS</text>
-                        <text class="borderLikeOrPass">CONNECT</text>
-                      </Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item className="carouselItem">
-                      <Image
-                        className="d-block w-100 carouselImage"
-                        src="https://www.liveabout.com/thmb/jyBQcDiXM_ysKfIHu88GvvW-20U=/1300x866/filters:no_upscale():max_bytes(150000):strip_icc()/loan-599c450a03f40200117e717a.jpg"
-                        alt="Second slide"
-                        fluid
-                      />
-                      <Carousel.Caption>
-                        <text className="borderLikeOrPass">PASS</text>
-                        <text class="borderLikeOrPass">CONNECT</text>
-                      </Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item className="carouselItem">
-                      <Image
-                        className="d-block w-100 carouselImage"
-                        src="https://i.ytimg.com/vi/3PjV1PQixGI/maxresdefault.jpg"
-                        alt="Third slide"
-                        fluid
-                      />
-                      <Carousel.Caption>
-                        <text className="borderLikeOrPass">PASS</text>
-                        <text class="borderLikeOrPass">CONNECT</text>
-                      </Carousel.Caption>
-                    </Carousel.Item>
-                  </Carousel>
-                </div>
-                <div class="col-sm-12 col-lg-6 bioContainer">
-                  <text class="bio">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Integer faucibus nec quam non viverra. Vivamus ultrices
-                    augue a velit ullamcorper placerat. Ut vel lectus tortor.
-                    Vestibulum consequat dui a condimentum pulvinar. Vestibulum
-                    ac congue mi. Sed iaculis luctus placerat. Sed nec mauris
-                    condimentum, lobortis purus et, bibendum ex. Mauris
-                    scelerisque rhoncus quam, et lobortis libero dictum sit
-                    amet. Vivamus scelerisque est sem, a elementum est bibendum
-                    et. Aenean sit amet ornare arcu. Aliquam id enim neque.
-                    Nulla blandit, enim vitae dignissim sagittis, sapien nisl
-                    finibus diam, ac convallis nisi arcu ut lorem. Curabitur
-                    viverra a urna non faucibus. Mauris eu enim mattis, commodo
-                    nibh non, posuere felis. Nulla facilisi. Proin sagittis
-                    neque a ligula efficitur, vel malesuada massa commodo.
-                    Phasellus iaculis, sem ut auctor sollicitudin, elit risus
-                    dictum quam, et volutpat augue lacus ut lectus. Mauris
-                    dignissim pretium fermentum. Morbi fermentum erat ac velit
-                    tincidunt sollicitudin. Donec gravida ipsum elit, at luctus
-                    massa aliquam sed. Ut tristique magna vitae magna
-                    consectetur, sed congue leo consectetur. Quisque in viverra
-                    justo. Etiam vestibulum dolor eu odio suscipit, ac sodales
-                    augue tincidunt. Proin porta, purus non hendrerit volutpat,
-                    lacus eros gravida sapien, nec aliquam sapien quam fringilla
-                    lectus. Nullam non mauris nibh. Suspendisse efficitur eros
-                    non turpis vestibulum commodo. Donec venenatis neque vel
-                    dapibus efficitur.
-                  </text>
-                </div>
-              </div>
-            </div>
-            <div class="row-2 likeOrPass"></div>
-          </div>
-        </div>
-      </div>
-    </div>;
-
     return (
-      <div className="main no-gutters h-100">
-        <Row className="h-100">
-        <Col xs={3} className="navigation bigRightBorder">
-          <Row className="top no-gutters topLeftNavigation">
-            <img class="menuIcon" src={menuIcon} />
-          </Row>
-          <Row className="bottom no-gutters">
-            <div class="row no-gutters likesOrMessages">
-              <div class=" col-sm-12 col-md-6 no-gutters bigRightBorder">
-                <text class="center bold">
-                  Likes<text class="numLikes">1</text>
-                </text>
-              </div>
-              <div class="col-sm-12 col-md-6 no-gutters bigRightBorder">
-                <text class="center">
-                  Messages <text class="numLikes">99+</text>
-                </text>
-              </div>
-            </div>
-            <div class="row likesMessages bigRightBorder">
-              Space for Likes and Messages
-            </div>
-          </Row>
-        </Col>
-        <Col xs={9}>
-          <Row className="top no-gutters menuContainer">
-            <div class="row logoAndMode">
-              <div class="col-7 logo">CancerChat</div>
-              <div class="col-1"></div>
-              <div class="col-1">
+      <div className="main no-gutters">
+        <Row className="no-gutters h-100">
+          <Col xs={3} className="navigation w-100 no-gutters">
+            <Row className="top no-gutters topLeftNavigation">
+              <img class="menuIcon ml-4" src={menuIcon} />
+            </Row>
+            <Row className="bottom no-gutters">
+              <Col className="no-gutters w-100">
+                <Row className="no-gutters likesOrMessages">
+                  <Col xs={12} md={6} className="no-gutters">
+                    <text class="center bold">
+                      Likes<text class="numLikes">1</text>
+                    </text>
+                  </Col>
+                  <Col xs={12} md={6} className="no-gutters bigLeftBorder">
+                    <text class="center">
+                      Messages <text class="numLikes">99+</text>
+                    </text>
+                  </Col>
+                </Row>
+                <Row className="likesMessages no-gutters">
+                  Space for Likes and Messages
+                </Row>
+              </Col>
+            </Row>
+          </Col>
+          <Col xs={9} className="bigLeftBorder">
+            <Row className="top no-gutters topRightNavigation">
+              <Col xs={7} className="logo">
+                CancerChat
+              </Col>
+              <Col xs={1}></Col>
+              <Col xs={1}>
                 <img class="mentorIcon" src={mentorIcon} />
-              </div>
-              <div class="col-1">
+              </Col>
+              <Col xs={1}>
                 <img class="heartIcon" src={heartIcon} />
-              </div>
-              <div class="col-2"></div>
-            </div>
-          </Row>
-          <Row className="bottom no-gutters"> 
-            <div class="row-10 bioAndPhoto">
-              <div class="row no-gutters h-100">
-                <div class="col-sm-12 col-lg-6 flex">
-                  <Carousel className="carousel" interval={null}>
-                    <Carousel.Item className="carouselItem">
-                      <Image
-                        className="d-block w-100 carouselImage"
-                        src="https://images.pexels.com/photos/3226584/pexels-photo-3226584.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-                        alt="First slide"
-                        fluid
-                      />
-                      <Carousel.Caption>
-                        <text className="borderLikeOrPass">PASS</text>
-                        <text class="borderLikeOrPass">CONNECT</text>
-                      </Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item className="carouselItem">
-                      <Image
-                        className="d-block w-100 carouselImage"
-                        src="https://www.liveabout.com/thmb/jyBQcDiXM_ysKfIHu88GvvW-20U=/1300x866/filters:no_upscale():max_bytes(150000):strip_icc()/loan-599c450a03f40200117e717a.jpg"
-                        alt="Second slide"
-                        fluid
-                      />
-                      <Carousel.Caption>
-                        <text className="borderLikeOrPass">PASS</text>
-                        <text class="borderLikeOrPass">CONNECT</text>
-                      </Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item className="carouselItem">
-                      <Image
-                        className="d-block w-100 carouselImage"
-                        src="https://i.ytimg.com/vi/3PjV1PQixGI/maxresdefault.jpg"
-                        alt="Third slide"
-                        fluid
-                      />
-                      <Carousel.Caption>
-                        <text className="borderLikeOrPass">PASS</text>
-                        <text class="borderLikeOrPass">CONNECT</text>
-                      </Carousel.Caption>
-                    </Carousel.Item>
-                  </Carousel>
-                </div>
-                <div class="col-sm-12 col-lg-6 bioContainer">
-                  <text class="bio">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Integer faucibus nec quam non viverra. Vivamus ultrices
-                    augue a velit ullamcorper placerat. Ut vel lectus tortor.
-                    Vestibulum consequat dui a condimentum pulvinar. Vestibulum
-                    ac congue mi. Sed iaculis luctus placerat. Sed nec mauris
-                    condimentum, lobortis purus et, bibendum ex. Mauris
-                    scelerisque rhoncus quam, et lobortis libero dictum sit
-                    amet. Vivamus scelerisque est sem, a elementum est bibendum
-                    et. Aenean sit amet ornare arcu. Aliquam id enim neque.
-                    Nulla blandit, enim vitae dignissim sagittis, sapien nisl
-                    finibus diam, ac convallis nisi arcu ut lorem. Curabitur
-                    viverra a urna non faucibus. Mauris eu enim mattis, commodo
-                    nibh non, posuere felis. Nulla facilisi. Proin sagittis
-                    neque a ligula efficitur, vel malesuada massa commodo.
-                    Phasellus iaculis, sem ut auctor sollicitudin, elit risus
-                    dictum quam, et volutpat augue lacus ut lectus. Mauris
-                    dignissim pretium fermentum. Morbi fermentum erat ac velit
-                    tincidunt sollicitudin. Donec gravida ipsum elit, at luctus
-                    massa aliquam sed. Ut tristique magna vitae magna
-                    consectetur, sed congue leo consectetur. Quisque in viverra
-                    justo. Etiam vestibulum dolor eu odio suscipit, ac sodales
-                    augue tincidunt. Proin porta, purus non hendrerit volutpat,
-                    lacus eros gravida sapien, nec aliquam sapien quam fringilla
-                    lectus. Nullam non mauris nibh. Suspendisse efficitur eros
-                    non turpis vestibulum commodo. Donec venenatis neque vel
-                    dapibus efficitur.
-                  </text>
-                </div>
-              </div>
-            </div>
-          </Row>
-        </Col>
+              </Col>
+              <Col xs={2}></Col>
+            </Row>
+            <Row xs={10} className="bottom no-gutters bioAndPhoto">
+              <Col xs={12} lg={6} className="flex">
+                <Carousel className="carousel" interval={null}>
+                  <Carousel.Item className="carouselItem">
+                    <Image
+                      className="d-block w-100 carouselImage"
+                      src="https://images.pexels.com/photos/3226584/pexels-photo-3226584.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+                      alt="First slide"
+                      fluid
+                    />
+                    <Carousel.Caption>
+                      <text className="borderLikeOrPass">PASS</text>
+                      <text class="borderLikeOrPass">CONNECT</text>
+                    </Carousel.Caption>
+                  </Carousel.Item>
+                  <Carousel.Item className="carouselItem">
+                    <Image
+                      className="d-block w-100 carouselImage"
+                      src="https://www.liveabout.com/thmb/jyBQcDiXM_ysKfIHu88GvvW-20U=/1300x866/filters:no_upscale():max_bytes(150000):strip_icc()/loan-599c450a03f40200117e717a.jpg"
+                      alt="Second slide"
+                      fluid
+                    />
+                    <Carousel.Caption>
+                      <text className="borderLikeOrPass">PASS</text>
+                      <text class="borderLikeOrPass">CONNECT</text>
+                    </Carousel.Caption>
+                  </Carousel.Item>
+                  <Carousel.Item className="carouselItem">
+                    <Image
+                      className="d-block w-100 carouselImage"
+                      src="https://i.ytimg.com/vi/3PjV1PQixGI/maxresdefault.jpg"
+                      alt="Third slide"
+                      fluid
+                    />
+                    <Carousel.Caption>
+                      <text className="borderLikeOrPass">PASS</text>
+                      <text class="borderLikeOrPass">CONNECT</text>
+                    </Carousel.Caption>
+                  </Carousel.Item>
+                </Carousel>
+              </Col>
+              <Col xs={12} lg={6} className="bioContainer">
+                <text class="bio">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Integer faucibus nec quam non viverra. Vivamus ultrices augue
+                  a velit ullamcorper placerat. Ut vel lectus tortor. Vestibulum
+                  consequat dui a condimentum pulvinar. Vestibulum ac congue mi.
+                  Sed iaculis luctus placerat. Sed nec mauris condimentum,
+                  lobortis purus et, bibendum ex. Mauris scelerisque rhoncus
+                  quam, et lobortis libero dictum sit amet. Vivamus scelerisque
+                  est sem, a elementum est bibendum et. Aenean sit amet ornare
+                  arcu. Aliquam id enim neque. Nulla blandit, enim vitae
+                  dignissim sagittis, sapien nisl finibus diam, ac convallis
+                  nisi arcu ut lorem. Curabitur viverra a urna non faucibus.
+                  Mauris eu enim mattis, commodo nibh non, posuere felis. Nulla
+                  facilisi. Proin sagittis neque a ligula efficitur, vel
+                  malesuada massa commodo. Phasellus iaculis, sem ut auctor
+                  sollicitudin, elit risus dictum quam, et volutpat augue lacus
+                  ut lectus. Mauris dignissim pretium fermentum. Morbi fermentum
+                  erat ac velit tincidunt sollicitudin. Donec gravida ipsum
+                  elit, at luctus massa aliquam sed. Ut tristique magna vitae
+                  magna consectetur, sed congue leo consectetur. Quisque in
+                  viverra justo. Etiam vestibulum dolor eu odio suscipit, ac
+                  sodales augue tincidunt. Proin porta, purus non hendrerit
+                  volutpat, lacus eros gravida sapien, nec aliquam sapien quam
+                  fringilla lectus. Nullam non mauris nibh. Suspendisse
+                  efficitur eros non turpis vestibulum commodo. Donec venenatis
+                  neque vel dapibus efficitur.
+                </text>
+              </Col>
+            </Row>
+          </Col>
         </Row>
       </div>
     );
